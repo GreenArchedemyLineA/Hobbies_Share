f54f548a04ea598f807a9733827bea49bb442936
hand-jui <hand.jurajura@gmail.com>
2023-05-02 01:21:02 +0900

    로그인창  css 수정

--- src/main/java/com/tenco/hobby/dto/JoinUpFormDto.java
+++ src/main/java/com/tenco/hobby/dto/JoinUpFormDto.java
@@ -3,8 +3,6 @@ package com.tenco.hobby.dto;
 import java.sql.Date;
 import java.sql.Timestamp;
 
-import javax.validation.constraints.Size;
-
 import com.tenco.hobby.enums.UserRole;
 
 import lombok.Data;
@@ -13,12 +11,11 @@ import lombok.Data;
 public class JoinUpFormDto {
 
 	private String email;
-	
+
 	private boolean checkEmail;
 
 	private String password;
 	private String username;
-	@Size(min = 2, message = "닉네임은 2자 이상 입력해주세요.")
 	private String nickname;
 	private Date birth;
 	private String phone;
